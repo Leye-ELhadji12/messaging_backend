3bef2c7dd432497881e0ba0a6d8697fbb6e3e1e6
Leye-ELhadji12 <leye.el-hadji@ugb.edu.sn>
2025-05-11 15:29:16 -0700

    remove mapper dependecies and completed MessageMapper implementing

--- src/main/java/com/messaging/mapper/MessageMapper.java
+++ src/main/java/com/messaging/mapper/MessageMapper.java
@@ -4,7 +4,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.messaging.dto.MessageDTO;
+import com.messaging.entity.ChatRoom;
 import com.messaging.entity.Message;
+import com.messaging.entity.User;
 
 @Service
 public class MessageMapper {
@@ -24,6 +26,16 @@ public class MessageMapper {
       public Message toMessage(MessageDTO messageDTO) {
             Message message = new Message();
             BeanUtils.copyProperties(messageDTO, message);
+            if (messageDTO.getChatRoomID() != null) {
+                  ChatRoom chatRoom = new ChatRoom();
+                  chatRoom.setId(messageDTO.getChatRoomID());
+                  message.setChatRoom(chatRoom);
+            }
+            if (messageDTO.getSender() != null) {
+                  User user = new User();
+                  user.setUsername(messageDTO.getSender());
+                  message.setUser(user);
+            }
             return message;
       }
 
