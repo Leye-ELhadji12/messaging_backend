e4d6fde0c91a5e66759c93aa3878053b20a5b13f
Leye-ELhadji12 <leye.el-hadji@ugb.edu.sn>
2025-05-12 07:02:47 -0700

    message service

--- src/main/java/com/messaging/repository/MessageRepository.java
+++ src/main/java/com/messaging/repository/MessageRepository.java
@@ -1,5 +1,6 @@
 package com.messaging.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.messaging.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, UUID>{
-
+      /**
+       * Finds the last message sent in a chat room.
+       * 
+       * @param chatRoomId the ID of the chat room
+       * @return the last message sent in the chat room, or null if no messages exist
+       */
+      Optional<Message> findTopByChatRoomIdOrderByDateSentMessageDesc(UUID chatRoomId);
 }
--- src/main/java/com/messaging/service/MessageService.java
+++ src/main/java/com/messaging/service/MessageService.java
@@ -1,9 +1,16 @@
 package com.messaging.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.messaging.dto.MessageDTO;
+import com.messaging.entity.Message;
 import com.messaging.mapper.MessageMapper;
 import com.messaging.repository.MessageRepository;
+import com.messaging.exception.MessageNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,5 +20,21 @@ public class MessageService {
       private final MessageMapper messageMapper;
       private final MessageRepository messageRepository;
 
-      
+      public MessageDTO lastMessage(UUID chatRoomUuid) {
+            Optional<Message> lastmessage = messageRepository.findTopByChatRoomIdOrderByDateSentMessageDesc(chatRoomUuid);
+            return lastmessage.map(messageMapper::toMessageDTO)
+                        .orElseThrow(() -> new MessageNotFoundException(chatRoomUuid +" not found"));
+      }
+
+      public List<MessageDTO> messagesByChatRoom(UUID chatRoomId) {
+            List<MessageDTO> messagesRoom = messageRepository.findById(chatRoomId).stream()
+                        .map(messageMapper::toMessageDTO)
+                        .toList();
+            return messagesRoom;
+      }
+
+      public MessageDTO saveMessage(MessageDTO messageDTO) {
+            Message message = messageMapper.toMessage(messageDTO);
+            return messageMapper.toMessageDTO(messageRepository.save(message));
+      }
 }
